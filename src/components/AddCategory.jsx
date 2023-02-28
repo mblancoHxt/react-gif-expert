3e74807564c5fcfcf2a46ba6f6d48211962db4a1
Blanco Matías <matias.blanco@flexibility.com.ar>
2023-02-28 11:00:37 -0300

    First Commit

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+
+export const AddCategory = ({ onNewCategory }) => {
+
+    const [inputValue, setInputValue] = useState('')
+
+    const onInputChange = ({ target }) => {
+        setInputValue(target.value)
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if (inputValue.trim().length <= 1) return;
+        // onNewCategory(categories => [...categories, inputValue]);
+        onNewCategory(inputValue.trim())
+        setInputValue('');
+    }
+
+    return (
+        <form onSubmit={(event) => onSubmit(event)}>
+            <input
+                type="text"
+                placeholder="Buscar Gif"
+                value={inputValue}
+                onChange={(event) => onInputChange(event)}>
+            </input>
+        </form>
+    )
+}
